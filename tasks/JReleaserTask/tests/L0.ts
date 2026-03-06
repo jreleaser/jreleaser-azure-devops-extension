be0df1676b784453b7ef4195d919069145cede00
shblue21 <shblue21@naver.com>
2026-03-06 18:27:03 +0900

    chore(task): trim invoker dependencies

--- tasks/JReleaserTask/tests/L0.ts
+++ tasks/JReleaserTask/tests/L0.ts
@@ -1,14 +1,8 @@
-import * as path from 'path';
-import * as ttm from 'azure-pipelines-task-lib/mock-test';
-import * as Mocha from 'mocha';
-import { expect, assert } from 'chai';
+import * as assert from 'node:assert/strict';
+import { describe, it } from 'node:test';
 
-describe('JReleaserTask L0 Suite', function () {
-  before(function () {});
-
-  after(() => {});
-
-  it('Does a basic hello world test', function (done: Mocha.Done) {
-    done();
+describe('JReleaserTask L0 Suite', () => {
+  it('Does a basic hello world test', () => {
+    assert.ok(true);
   });
 });
